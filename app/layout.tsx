796276b4407326a55f591c683f1e99e6faf09c2e
Muhammed Muavviz <muhammedmuavviz@gmail.com>
2026-06-04 23:05:42 +0530

    Updated favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,6 @@ const playfair = Playfair_Display({
 export const metadata: Metadata = {
   title: "Delonti | Innovating RFID, IoT & Cybersecurity",
   description: "Innovating with RFID, IoT & Cybersecurity Across Government & Industry",
-  icons: {
-    icon: "/fav-test.png",
-  },
 };
 
 export default function RootLayout({
