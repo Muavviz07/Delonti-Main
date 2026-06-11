b470d0a7647be8a3fadc1cfc1c6563cc6297e2bf
Muhammed Muavviz <muhammedmuavviz@gmail.com>
2026-06-11 11:07:04 +0530

    updated titles in sols vids

--- components/VideoGallery.tsx
+++ components/VideoGallery.tsx
@@ -11,18 +11,18 @@ interface Video {
 }
 
 const videos: Video[] = [
-    { id: "1153765212", title: "Utilities - RFID Demo", thumbnail: "https://i.vimeocdn.com/video/2105842220-2de14a5c364b840c17195e6fa9b452f5fc88181a6a3b1fe76ecb839571127d6f-d_640?region=us" },
-    { id: "1153764948", title: "Utilities - RFID Demo", thumbnail: "https://i.vimeocdn.com/video/2105841877-c12bab9c65a9e841998751e47bb020bcd55f19bb45c09c2934e929d1a5e792b2-d_640?region=us" },
-    { id: "1153764659", title: "Transportation - RFID Demo", thumbnail: "https://i.vimeocdn.com/video/2105841557-bc2bed09b942179c15f9f129eaef02e504ae4acf40332226f624097b0099add6-d_640?region=us" },
-    { id: "1153764350", title: "Schools and Universities - RFID Demo", thumbnail: "https://i.vimeocdn.com/video/2105841165-9f06a569b023176aa8d65df6dc541ae7f3ef67786de895e41548745b0e3a4afe-d_640?region=us" },
-    { id: "1153764043", title: "Resource Augmentation - RFID Demo", thumbnail: "https://i.vimeocdn.com/video/2105840727-ebb14a9ea32d889d13ba0a31e803ca99c3acd96b35264852ad543257bc85600a-d_640?region=us" },
-    { id: "1153763709", title: "Port - RFID Demo", thumbnail: "https://i.vimeocdn.com/video/2105840279-11bc80efe1def8c2724568b86ff50bcbf6257d81eab9fd5d785ec2449361b1b9-d_640?region=us" },
-    { id: "1153763472", title: "Indoor Navigation - RFID Demo", thumbnail: "https://i.vimeocdn.com/video/2105839861-dd3d695869f375ff2594e607bfbd5c0f0f9858818d5623ea60c1f04c71ba9bda-d_640?region=us" },
-    { id: "1153763159", title: "Healthcare - RFID Demo", thumbnail: "https://i.vimeocdn.com/video/2105839519-9df9b78a160b97fcd5a65375d192d39a9831e0caba841d52fd6f1cd40fb84d8f-d_640?region=us" },
-    { id: "1153762836", title: "Events and Festivals - RFID Demo", thumbnail: "https://i.vimeocdn.com/video/2105839021-3887f94b3227251ed8b57c320bbdd6e194293701909390f3a9c15b609e0d6336-d_640?region=us" },
-    { id: "1153762480", title: "Cybersecurity Demo Only", thumbnail: "https://i.vimeocdn.com/video/2105838579-4c5e71da9342b1b1c17f56646eec04ab4039d59bdbb879812ae26801f173704c-d_640?region=us" },
-    { id: "1153761741", title: "Homeless Kiosks Combined - RFID Demo", thumbnail: "https://i.vimeocdn.com/video/2105837587-a22a2b7030b8f6b1a5edc6cfa5a7bc6ddb5023fbf2589521d4ca6ca85d533278-d_640?region=us" },
-    { id: "1153761078", title: "Cities and Counties - RFID Demo", thumbnail: "https://i.vimeocdn.com/video/2105836651-a15983c4c61296154db174b66b8133acedfbe34fa9e69e66c5ecd8e09bea135a-d_640?region=us" },
+    { id: "1153765212", title: "Utilities - 1", thumbnail: "https://i.vimeocdn.com/video/2105842220-2de14a5c364b840c17195e6fa9b452f5fc88181a6a3b1fe76ecb839571127d6f-d_640?region=us" },
+    { id: "1153764948", title: "Utilities - 2", thumbnail: "https://i.vimeocdn.com/video/2105841877-c12bab9c65a9e841998751e47bb020bcd55f19bb45c09c2934e929d1a5e792b2-d_640?region=us" },
+    { id: "1153764659", title: "Transportation", thumbnail: "https://i.vimeocdn.com/video/2105841557-bc2bed09b942179c15f9f129eaef02e504ae4acf40332226f624097b0099add6-d_640?region=us" },
+    { id: "1153764350", title: "Schools and Universities", thumbnail: "https://i.vimeocdn.com/video/2105841165-9f06a569b023176aa8d65df6dc541ae7f3ef67786de895e41548745b0e3a4afe-d_640?region=us" },
+    { id: "1153764043", title: "Resource Augmentation", thumbnail: "https://i.vimeocdn.com/video/2105840727-ebb14a9ea32d889d13ba0a31e803ca99c3acd96b35264852ad543257bc85600a-d_640?region=us" },
+    { id: "1153763709", title: "Port", thumbnail: "https://i.vimeocdn.com/video/2105840279-11bc80efe1def8c2724568b86ff50bcbf6257d81eab9fd5d785ec2449361b1b9-d_640?region=us" },
+    { id: "1153763472", title: "Indoor Navigation", thumbnail: "https://i.vimeocdn.com/video/2105839861-dd3d695869f375ff2594e607bfbd5c0f0f9858818d5623ea60c1f04c71ba9bda-d_640?region=us" },
+    { id: "1153763159", title: "Healthcare", thumbnail: "https://i.vimeocdn.com/video/2105839519-9df9b78a160b97fcd5a65375d192d39a9831e0caba841d52fd6f1cd40fb84d8f-d_640?region=us" },
+    { id: "1153762836", title: "Events and Festivals", thumbnail: "https://i.vimeocdn.com/video/2105839021-3887f94b3227251ed8b57c320bbdd6e194293701909390f3a9c15b609e0d6336-d_640?region=us" },
+    { id: "1153762480", title: "Cybersecurity", thumbnail: "https://i.vimeocdn.com/video/2105838579-4c5e71da9342b1b1c17f56646eec04ab4039d59bdbb879812ae26801f173704c-d_640?region=us" },
+    { id: "1153761741", title: "Homeless Kiosks Combined", thumbnail: "https://i.vimeocdn.com/video/2105837587-a22a2b7030b8f6b1a5edc6cfa5a7bc6ddb5023fbf2589521d4ca6ca85d533278-d_640?region=us" },
+    { id: "1153761078", title: "Cities and Counties", thumbnail: "https://i.vimeocdn.com/video/2105836651-a15983c4c61296154db174b66b8133acedfbe34fa9e69e66c5ecd8e09bea135a-d_640?region=us" },
 ];
 
 export default function VideoGallery() {
